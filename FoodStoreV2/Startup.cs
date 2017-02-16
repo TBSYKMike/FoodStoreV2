d6d0d4d981454532a99e395955f44605128602a8
Mike <Mike@Mike-Dator>
2017-02-16 12:41:27 +0100

    Add project files.

---
+++ FoodStoreV2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodStoreV2.Startup))]
+namespace FoodStoreV2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
