3bf3ee218a8af05bd0018ac9fae5b77b06d741e5
Akasuna <kim28138th@gmail.com>
2017-02-23 15:53:26 +0100

    Added some comments and cleanup

--- FoodStoreV2/CSharpClasses/SendMail.cs
+++ FoodStoreV2/CSharpClasses/SendMail.cs
@@ -16,16 +16,20 @@ namespace FoodStoreV2.CSharpClasses
         {
             try
             {
+                //Creates a new MailMessage object
                 MailMessage msg = new MailMessage();
 
+                //adds the subject, reciever, sender and body to the mail
                 msg.To.Add(reciever);
                 msg.From = new MailAddress("thatchat12345@gmail.com");
                 msg.Subject = subject;
                 msg.Body = message;
                 msg.IsBodyHtml = true;
 
+                //Sends the mail via the gmail smtp server
                 SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
                 smtpClient.EnableSsl = true;
+                //Sets the credentials for our mail
                 smtpClient.Credentials = new System.Net.NetworkCredential("thatchat12345@gmail.com", "TraktorBengt123");
                 smtpClient.Send(msg);
                 
