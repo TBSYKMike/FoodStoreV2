a5ee9cfe733d779600b80270c8d969f9f0676f89
Jocke90 <roslund1990@gmail.com>
2017-02-23 13:25:43 +0100

    asdf

--- FoodStoreV2/WebForms/GetHint.aspx.cs
+++ FoodStoreV2/WebForms/GetHint.aspx.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FoodStoreV2.CSharpClasses;
+
 
 namespace FoodStoreV2.WebForms
 {
     public partial class getHint : System.Web.UI.Page
     {
+        List<String> nameList = new List<String>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected List<String> fillList()
+        {
+            DatabaseConnector databaseConnector = new DatabaseConnector();
+            List<Product> list = databaseConnector.getProductsOnCategory(0);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                nameList.Add(list[i].getName());
+            }
+
+            return nameList;
+        }
+
     }
 }
