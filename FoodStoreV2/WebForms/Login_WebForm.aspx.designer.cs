25a16cf5bf67e177644b92d2531a528110d805b6
Jocke90 <roslund1990@gmail.com>
2017-02-23 16:03:26 +0100

    Added ajax info text onclick

--- FoodStoreV2/WebForms/Login_WebForm.aspx.designer.cs
+++ FoodStoreV2/WebForms/Login_WebForm.aspx.designer.cs
@@ -110,5 +110,23 @@ namespace FoodStoreV2.WebForms {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label robotLabel;
+        
+        /// <summary>
+        /// ButtonInfo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonInfo;
+        
+        /// <summary>
+        /// LabelInfo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelInfo;
     }
 }
